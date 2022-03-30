b74e9556e872452417d3c143551597a9d7ed3b8f
informatico32 <informatico32@gmail.com>
2022-03-29 21:56:13 -0400

    ProyectoCompletadov2
    ProyectoCompletadov2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoConsumoFinanciero';
- 
 
-
+public iscollapsed=false;
+public isMenuCollapsed = true;
 }
